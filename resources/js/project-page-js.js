1bc91e251c34805711829a036131df64ec2eb246
tienthanh-pham <phamtienthanh1996@gmail.com>
2019-06-22 16:40:56 +0700

    [personal-project] update personal project
    [1] organize the folder
    [2] update the main personal project page
    [3] create the popup menu for each project

---
+++ resources/js/project-page-js.js
@@ -0,0 +1,34 @@
+var currentModel;
+
+var modelList = [
+    "clipboardIPC",
+    "autotunePID"
+]
+
+function init() {
+    let i = 0
+    for (i = 0; i < modelList.length; i++) {
+        document.getElementById(modelList[i]).onclick = function () {
+            displayModel(document.getElementById(this.id + "-model"))
+        }
+        document.getElementsByClassName("close")[i].onclick = function(){
+            currentModel.style.display = "none";
+        }
+    }
+}
+
+function displayModel(_modal) {
+    currentModel = _modal
+    _modal.style.display = "block";
+}
+
+/* disable close model by click on window feature
+window.onclick = function(event) {
+  if (event.target == currentModel) {
+    currentModel.style.display = "none";
+  }
+}*/
+
+window.onload = function () {
+    init();
+}
